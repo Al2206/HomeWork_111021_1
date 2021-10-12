bf5327b095cc8d5ee8b264b712be5ab55e47a710
Al2206 <77494086+Al2206@users.noreply.github.com>
2021-10-12 23:27:11 +0300

    Update Task_3.java

--- src/HomeWork/Task_3.java
+++ src/HomeWork/Task_3.java
@@ -1,24 +1,18 @@
 package HomeWork;
 
 public class Task_3 {
-    public static void main(String[] args) {
-       int a = 12;
-       int b = 12;
-       int c = 19;
-       int d = -7;
-        boolean e;
-        e= a >= b;// true
-        e= a <= b;// true
-        e= a >= c;// false
-        e= a <= c;// true
-        e= a >= d;// true
-        e= a <= d;// false
-        e= b >= c;// false
-        e= b <= c;// true
-        e= b >= d;// true
-        e= b <= d;// false
-        e= c >= d;// true
-        e= c <= d;// falce
-        System.out.print(" Ответ: " + e);
+
+ public static void main(String[] args) {
+    int a =12;
+    int b = 12;
+    int c = 19;
+    int d = -7;
+    if (a > b && a >c && a > d) {
+        System.out.println( " Ответ: большее число - " + a );
+    } else if (b > c && b > d) {
+        System.out.println(" Ответ: большее число - " + b );
+    } else if (c > d) {
+        System.out.println(" Ответ: большее число - " + c );
     }
+ }
 }
