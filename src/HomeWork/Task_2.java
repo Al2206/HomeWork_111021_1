0fdd097fcc6d79de499cd944e26d179ee77b8dfe
Al2206 <77494086+Al2206@users.noreply.github.com>
2021-10-12 23:25:54 +0300

    Update Task_2.java

--- src/HomeWork/Task_2.java
+++ src/HomeWork/Task_2.java
@@ -2,13 +2,15 @@ package HomeWork;
 
 public class Task_2 {
     public static void main(String[] args) {
-   int a = 12;
-   int b = 8;
-    boolean c = a < b;// false
-        System.out.print(" Ответ1: " + c);
-        boolean d = a > b;// true
-        System.out.print(" Ответ2: " + d);
-}
+        int a =12;
+        int b = 8;
+        if (a > b) {
+            System.out.println( " Ответ: большее число - " + a );
+        } else if (a < b) {
+            System.out.println(" Ответ: большее число - " + b );
+        } else if (a == b) {
+            System.out.println(" Числа равны " );
+        }
+    }
 
 }
-
